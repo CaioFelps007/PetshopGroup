0bd25a2e95b7b06a6a4490df84679b16b6176521
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-05-18 22:40:11 -0300

    rapidin

--- DogsAdocaoProject/src/pages/Inicio.js
+++ DogsAdocaoProject/src/pages/Inicio.js
@@ -6,38 +6,60 @@ import Textos from "../components/TextComponents";
 import { useFonts } from "expo-font";
 
 export default function PageInicial() {
+  const [fontsLoaded] = useFonts({
+    TitanOne: require("../assets/fonts/TitanOne-Regular.ttf"),
+  });
 
-    const [fontsLoaded] = useFonts({
-        "TitanOne": require("../assets/fonts/TitanOne-Regular.ttf"),
-    });
+  if (!fontsLoaded) {
+    return undefined;
+  }
 
-    if (!fontsLoaded) {
-        return undefined;
-    }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.boasVindas}>
+        <Textos
+          title={"Welcome"}
+          estilotexto={{ fontFamily: "TitanOne", fontSize: 26 }}
+        />
+        <Textos
+          title={"PETlove"}
+          estilotexto={{ fontFamily: "TitanOne", fontSize: 41 }}
+        />
+        <Textos
+          title={"Venha conheçer seu novo amigo cão..."}
+          estilotexto={styles.subtitulo}
+        />
+      </View>
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.boasVindas}>
-                <Textos title={"Welcome"} estilotexto={{ fontFamily: "TitanOne", fontSize: 20 }} />
-                <Textos title={"Amicão"} estilotexto={{ fontFamily: "TitanOne", fontSize: 37 }} />
-                <Textos title={"Venha conheçer seu novo amigo cão..."} estilotexto={styles.subtitulo} />
-            </View>
+      <View style={styles.ViewImage}>
+        <Image
+          source={require("../assets/images/dobermanInicio.png")}
+          style={styles.imagemInicial}
+        />
+      </View>
 
-            <View style={styles.ViewImage}>
-                <Image source={require('../assets/images/cachorrinsorri.png')} style={styles.imagemInicial} />
-            </View>
-
-            <View style={styles.ButtonsInicial}>
-                <Botao TxtBtn={"Entrar"} estilos={styles.buttonInicial} txtestilos={styles.conteudobotao} />
-                <Botao TxtBtn={"Cadastro"} estilos={[styles.buttonInicial, {
-                    backgroundColor: '#EFB758', shadowOffset: { width: 0, height: 5 },
-                    shadowColor: 'gold',
-                    shadowRadius: 10,
-                    shadowOpacity: 0.5,
-                    elevation: 5
-                }]} txtestilos={[styles.conteudobotao, { color: 'black' }]} />
-            </View>
-
-        </SafeAreaView>
-    )
+      <View style={styles.ButtonsInicial}>
+        <Botao
+          TxtBtn={"Entrar"}
+          estilos={styles.buttonInicial}
+          txtestilos={styles.conteudobotao}
+        />
+        <Botao
+          TxtBtn={"Cadastro"}
+          estilos={[
+            styles.buttonInicial,
+            {
+              backgroundColor: "#EFB758",
+              shadowOffset: { width: 0, height: 5 },
+              shadowColor: "gold",
+              shadowRadius: 10,
+              shadowOpacity: 0.5,
+              elevation: 5,
+            },
+          ]}
+          txtestilos={[styles.conteudobotao, { color: "black" }]}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
--- DogsAdocaoProject/src/styles/StyleSheet.js
+++ DogsAdocaoProject/src/styles/StyleSheet.js
@@ -1,73 +1,81 @@
 import { StyleSheet } from "react-native";
 
-
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    // TELA INICIAL
-    boasVindas: {
-        width: '100%',
-        paddingLeft: 40,
+  // TELA INICIAL
+  boasVindas: {
+    width: "100%",
+    paddingLeft: 40,
+  },
 
-    },
+  tituloWelcome: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
 
-    tituloWelcome: {
-        fontSize: 25,
-        fontWeight: 'bold'
-    },
+  tituloName: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 
-    tituloName: {
-        fontSize: 30,
-        fontWeight: 'bold'
-    },
+  subtitulo: {
+    fontSize: 17,
+  },
 
-    subtitulo: {
-        fontSize: 17,
-    },
+  ViewImage: {
+    width: "100%",
+    height: 370,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 60,
+    marginBottom: 20,
+    borderRadius: 200,
+  },
 
-    ViewImage: {
-        width: '100%',
-        height: 350,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 25,
-        marginBottom: 10
-    },
+  imagemInicial: {
+    width: 290,
+    height: 350,
+    shadowOffset: { width: 0, height: 5 },
+    shadowColor: "blue",
+    shadowRadius: 10,
+    shadowOpacity: 0.5,
+    elevation: 5,
+    borderRadius: 200,
+  },
 
-    imagemInicial: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
-    },
+  ButtonsInicial: {
+    marginTop: 20,
+  },
 
-    ButtonsInicio: {
-        width: '100%',
-        height: 350,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  ButtonsInicio: {
+    width: "100%",
+    height: 350,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    buttonInicial: {
-        borderRadius: 7,
-        backgroundColor: '#17395C',
-        width: 325,
-        height: 45,
-        margin: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowOffset: { width: 0, height: 5 },
-        shadowColor: 'blue',
-        shadowRadius: 10,
-        shadowOpacity: 0.5,
-        elevation: 5
-    },
-    conteudobotao: {
-        color: 'white',
-        fontSize: 17
-    },
-})
+  buttonInicial: {
+    borderRadius: 7,
+    backgroundColor: "#17395C",
+    width: 325,
+    height: 45,
+    margin: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOffset: { width: 0, height: 5 },
+    shadowColor: "blue",
+    shadowRadius: 10,
+    shadowOpacity: 0.5,
+    elevation: 5,
+  },
+  conteudobotao: {
+    color: "white",
+    fontSize: 17,
+  },
+});
